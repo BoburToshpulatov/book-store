fe653ea72dcd4094896abb6007c84e66581f5a5f
Bobur Toshpulatov <bobur.vocal@gmail.ru>
2025-03-08 15:10:40 +0500

    first commit

--- src/routers/AdminRoute.jsx
+++ src/routers/AdminRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const AdminRoute = ({ children }) => {
   const token = localStorage.getItem("token");
   if (!token) {
-    return <Navigate to="/" />;
+    return <Navigate to="/admin" />;
   }
   return children ? children : <Outlet />;
 };
